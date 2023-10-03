fb1ef47df41dd687b50006a25323e003ece68924
Fznaf <fauzan.afaisal@gmail.com>
2023-10-04 00:10:25 +0700

    modify view

--- resources/views/test.blade.php
+++ resources/views/test.blade.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Bootstrap demo</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
-    <link href="/styles/style.css" rel="stylesheet">  
+    <link href="/styles/test.css" rel="stylesheet">  
   </head>
   <body>
 
@@ -44,11 +44,11 @@
             </div>
         </nav>
 
-        <img class="image-before" src="/images/image.png" alt="image">
+        <img class="image-before" source="../public/images/image.png" alt="image">
    <div class="article">
     <img class="" src="" alt="" width="" height=""> 
     <h1>Title</h1>
-    <img class="image-article" src="/images/image.png" alt="image">
+    <img class="image-article" source="../public/images/image.png" alt="image">
     "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
    </div>
    
