74bcf0bb817ea85593567f2fa1c9ca15f3ed1a6b
satriasuryap <115058337+satriasuryap@users.noreply.github.com>
2023-10-03 23:40:04 +0700

    Update 2023_10_03_161018_create_cards_table.php
    p

--- database/migrations/2023_10_03_161018_create_cards_table.php
+++ database/migrations/2023_10_03_161018_create_cards_table.php
@@ -14,6 +14,9 @@ return new class extends Migration
         Schema::create('cards', function (Blueprint $table) {
             $table->id();
             $table->timestamps();
+            $table->string('name');
+            $table->float('rate');
+            $table->binary('image');
         });
     }
 
