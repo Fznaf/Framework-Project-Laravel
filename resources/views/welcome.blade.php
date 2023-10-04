d3c39170c081a414f4c710e80b215c67d4fdd501
Fznaf <fauzan.afaisal@gmail.com>
2023-10-04 10:16:43 +0700

    modify view

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -43,15 +43,11 @@
                 </div>
             </div>
         </nav>
-        <div class="article article-background">
    <div class="article">
-    
-    <img class="" src="" alt="" width="" height=""> 
     <h1>Title</h1>
     <img class="image-article" src="/images/miriam.jpg" alt="image">
     "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
    </div>
-        </div>
 
    
     
